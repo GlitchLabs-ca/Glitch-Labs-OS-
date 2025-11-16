b7c00e48a2f95465beff3cf2750841a7e2d64459
Shay Sapozhnikov <45404072+ShaySapozhnikov@users.noreply.github.com>
2025-11-16 14:27:36 -0600

    working on suppabase Backend

--- OS/OS-Scripts/loginLogic.js
+++ OS/OS-Scripts/loginLogic.js
@@ -1,13 +1,33 @@
+
+
+//env
+
+require('dotenv').config();
+
+//supabase
+/////////////////////////////////////////////////////////////
+
+
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+/////////////////////////////////////////////////////////////
+
+
+
 const username = document.getElementById('username');
 const password = document.getElementById('password');
 const loginBtn = document.getElementById('loginBtn');
+
+
+// Get reference to the login overlay
 const overlay  = document.getElementById('LoginOverlay');
 
 var successfulLogin = false; // Placeholder for actual login status
 
 password.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
-        // alert(`Username: ${username.value}\nPassword: ${password.value}`);
+        alert(`Username: ${username.value}\nPassword: ${password.value}`);
         // add suppabase login logic here
     }
 });
